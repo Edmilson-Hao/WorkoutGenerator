78cebe2ce682fe1b8b45fd5b4f20405d7d768a16
Edmilson-Hao <edmilsonwp@gmail.com>
2020-12-08 02:11:52 +0000

    Change

--- src/_generateWorkout.js
+++ src/_generateWorkout.js
@@ -1,3 +1,37 @@
 generateFullBodyMainGroupDay = () => {
-    console.log("HI!");
+    var firstExercise = randomNumber();
+    var secondExercise;
+    var thirdExercise;
+
+    do {
+        secondExercise = randomNumber();
+    } while (secondExercise === firstExercise);
+
+    do {
+        thirdExercise = randomNumber();
+    } while (thirdExercise === secondExercise || thirdExercise === firstExercise);
+    
+    if (firstExercise === 1){
+        document.getElementById('generatedWorkout').style.display = 'block';
+        document.getElementById('generatedWorkout').innerHTML += `
+            <p>Peito</p>
+        `;
+    } else if (firstExercise === 2){
+        document.getElementById('generatedWorkout').style.display = 'block';
+        document.getElementById('generatedWorkout').innerHTML += `
+            <p>Back</p>
+        `;
+    } else if (firstExercise === 3){
+        document.getElementById('generatedWorkout').style.display = 'block';
+        document.getElementById('generatedWorkout').innerHTML += `
+            <p>Legs</p>
+        `;
+    }
+
+
+
+}
+
+randomNumber = () => {
+    return parseInt(1 + (4 - 1) * Math.random());
 }
