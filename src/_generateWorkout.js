5fd959ff567a0373faf61d87e2c39b8216e133da
edmilson <edmilsonwp>
2020-12-08 12:35:42 -0300

    Hail

--- src/_generateWorkout.js
+++ src/_generateWorkout.js
@@ -10,28 +10,193 @@ generateFullBodyMainGroupDay = () => {
     do {
         thirdExercise = randomNumber();
     } while (thirdExercise === secondExercise || thirdExercise === firstExercise);
-    
+
+    /* Firs Group is Chest
+    -----------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------- */
     if (firstExercise === 1){
+        document.getElementById('divWorkoutContainer').style.display = 'none';
         document.getElementById('generatedWorkout').style.display = 'block';
         document.getElementById('generatedWorkout').innerHTML += `
-            <p>Peito</p>
+            <table class='tableResult'>
+                <tr>
+                    <td>` + chest[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
         `;
+
+        if(secondExercise === 2) {
+            document.getElementById('generatedWorkout').innerHTML += `
+            <table class='tableResult'>
+                <tr>
+                    <td>` + back[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+
+                <tr>
+                    <td>` + legs[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
+        `;
+
+        } else if (secondExercise === 3) {
+            document.getElementById('generatedWorkout').innerHTML += `
+            <table class='tableResult'>
+                <tr>
+                    <td>` + legs[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+
+                <tr>
+                    <td>` + back[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
+        `;
+        }
+    /* Firs Group is Back
+    -----------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------- */
     } else if (firstExercise === 2){
+        document.getElementById('divWorkoutContainer').style.display = 'none';
         document.getElementById('generatedWorkout').style.display = 'block';
         document.getElementById('generatedWorkout').innerHTML += `
-            <p>Back</p>
+            <table class='tableResult'>
+                <tr>
+                    <td>` + back[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
         `;
+
+        if(secondExercise === 2) {
+            document.getElementById('generatedWorkout').innerHTML += `
+            <table class='tableResult'>
+                <tr>
+                    <td>` + chest[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+
+                <tr>
+                    <td>` + legs[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
+        `;
+
+        } else if (secondExercise === 3) {
+            document.getElementById('generatedWorkout').innerHTML += `
+            <table class='tableResult'>
+                <tr>
+                    <td>` + legs[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+
+                <tr>
+                    <td>` + chest[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
+        `;
+        }
+    /* Firs Group is Legs
+    -----------------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------- */
     } else if (firstExercise === 3){
+        document.getElementById('divWorkoutContainer').style.display = 'none';
         document.getElementById('generatedWorkout').style.display = 'block';
         document.getElementById('generatedWorkout').innerHTML += `
-            <p>Legs</p>
+            <table class='tableResult'>
+                <tr>
+                    <td>` + legs[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
         `;
+
+        if(secondExercise === 2) {
+            document.getElementById('generatedWorkout').innerHTML += `
+            <table class='tableResult'>
+                <tr>
+                    <td>` + back[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+
+                <tr>
+                    <td>` + chest[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
+        `;
+
+        } else if (secondExercise === 3) {
+            document.getElementById('generatedWorkout').innerHTML += `
+            <table class='tableResult'>
+                <tr>
+                    <td>` + chest[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+
+                <tr>
+                    <td>` + back[randomNumber()].name + `</td>
+                
+                    <td>3</td>
+                
+                    <td>12</td>
+                </tr>
+            </table>
+        `;
+        }
     }
-
-
-
 }
 
 randomNumber = () => {
-    return parseInt(1 + (4 - 1) * Math.random());
+    return parseInt(0 + (3 - 0) * Math.random());
 }
